570084e017d644fdbf5ccdaeede8aabfbae6ee61
CORP\SANTHOKR <kk.santhosh@capgemin.com>
2021-12-16 20:06:22 +0530

    test

--- src/Layout/Header.js
+++ src/Layout/Header.js
@@ -192,7 +192,7 @@ export class Header extends Component {
 
           <a className="nav-link nav-profile d-flex align-items-center pe-0" href="/" data-bs-toggle="dropdown">
             <img src="assets/img/profile-img.jpg" alt="Profile" className="rounded-circle"/>
-            <span className="d-none d-md-block dropdown-toggle ps-2">K. Anderson</span>
+            <span className="d-none d-md-block dropdown-toggle ps-2">Santhosh Ambekar</span>
           </a>
 
           <ul className="dropdown-menu dropdown-menu-end dropdown-menu-arrow profile">
